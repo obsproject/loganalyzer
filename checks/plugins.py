8fa05cc7f6e6c469b26012e5043fd04220aff6d2
gxalpha <beckmann.sebastian@outlook.de>
2021-06-13 15:36:09 +0200

    Change wording in check about StreamElements
    Changes wording in the check regarding StreamElements' obs.live plugin.
    
    Makes it a LEVEL_INFO and with wording less hostile than before, making
    it clear that while it can cause issues in certain scenarios, it isn't
    always at fault.

--- checks/plugins.py
+++ checks/plugins.py
@@ -4,5 +4,5 @@ from .utils.utils import *
 
 def checkElements(lines):
     if (len(search('obs-streamelements', lines)) > 0):
-        return [LEVEL_WARNING, "StreamElements OBS.Live",
-                """The obs.live plugin is installed. This overwrites OBS' default browser source and causes a severe performance impact. To get rid of it, first, export your scene collections and profiles, second manually uninstall OBS completely, third reinstall OBS Studio only with the latest installer from <a href="https://obsproject.com/download">https://obsproject.com/download</a>"""]
+        return [LEVEL_INFO, "StreamElements OBS.Live",
+                """StreamElements' OBS.live plugin is installed. This interferes with OBS' browser source and can cause performance impacts. If you want to remove it, follow their <a href="https://streamelements.elevio.help/en/articles/4-how-do-i-uninstall-the-obs-live-plugin">uninstall instructions</a> (make sure that "User Settings" is <b>not</b> selected), then reinstall OBS Studio using only the latest installer from <a href="https://obsproject.com/download">https://obsproject.com/download</a>."""]
